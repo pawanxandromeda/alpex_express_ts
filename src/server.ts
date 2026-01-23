82ac4cd31450d8d310d185d6879ce6f5396fe122
pawanxandromeda <pawanxandromeda@gmail.com>
2026-01-23 14:18:28 +0530

    gdggh

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import app from "./app";
 import prisma from "./config/postgres";
 
-
-const PORT = process.env.PORT || 5000;
-
-
+const PORT = Number(process.env.PORT) || 5000;
 
 async function startServer() {
   try {
@@ -12,13 +9,12 @@ async function startServer() {
     await prisma.$queryRaw`SELECT 1`;
     console.log("✅ Database connected successfully!");
 
-    // Start server only if DB is connected
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (err) {
     console.error("❌ Database connection failed:", err);
-    process.exit(1); // Exit if DB not connected
+    process.exit(1);
   }
 }
 
