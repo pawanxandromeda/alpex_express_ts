f09c4eda57ea980fa9cd164e9af56abe0fd872c1
pawanxandromeda <pawanxandromeda@gmail.com>
2026-01-25 01:49:16 +0530

    s

--- src/modules/accounts/accounts.routes.ts
+++ src/modules/accounts/accounts.routes.ts
@@ -11,6 +11,6 @@ router.get("/po/:poId/bills", protect as any,encryptResponse, controller.getBill
 router.post("/", protect as any, controller.createBill);
 router.post("/:billId/dispute", protect as any, controller.raiseDispute);
 router.post("/po/:poId/dispute", protect as any, controller.raisePoDispute);
-router.post("/po/:poId/sales-comment", protect as any, controller.addSalesComment);
+// router.post("/po/:poId/sales-comment", protect as any, controller.addSalesComment);
 
 export default router;
